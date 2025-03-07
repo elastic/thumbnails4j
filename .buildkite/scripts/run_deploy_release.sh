731520cee297d715be24ea50efb0f45b67d682fc
nassimkammah <nassim.kammah@elastic.co>
2025-03-07 19:42:14 +0100

    Reverting my experiment

--- .buildkite/scripts/run_deploy_release.sh
+++ .buildkite/scripts/run_deploy_release.sh
@@ -17,23 +17,6 @@ SCRIPT_PATH="$(dirname "${BASH_SOURCE}")"
 BUILDKITE_PATH=$(realpath "$(dirname "$SCRIPT_PATH")")
 PROJECT_ROOT=$(realpath "$(dirname "$BUILDKITE_PATH")")
 
-# Prepare a secure temp folder not shared between other jobs to store the key ring
-export TMP_WORKSPACE=/tmp/secured
-export KEY_FILE=$TMP_WORKSPACE"/private.key"
-chmod -R 700 $TMP_WORKSPACE
-
-# Signing keys
-GPG_SECRET=kv/ci-shared/release-eng/team-release-secrets/thumbnails4j/gpg
-
-vault kv get --field="keyring" $GPG_SECRET | base64 -d > $KEY_FILE
-# Import the key into the keyring
-echo "$KEYPASS_SECRET" | gpg --batch --import "$KEY_FILE"
-
-# Configure the committer since the maven release requires to push changes to GitHub
-# This will help with the SLSA requirements.
-git config --global user.email "${GIT_EMAIL}"
-git config --global user.name "${GIT_USER}"
-
 # Set Maven options
 export MAVEN_CONFIG="-V -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -Dhttps.protocols=TLSv1.2 -Dmaven.wagon.http.retryHandler.count=3 -Dhttp.keepAlive=false -Dmaven.wagon.http.pool=false $MAVEN_CONFIG"
 
